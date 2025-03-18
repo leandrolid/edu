6572d0c5b131293f56bad25d816ee6e06f9a848d
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-03-18 20:28:32 -0300

    chore: setup eslint, prettier and tsconfig

--- config/eslint/next.js
+++ config/eslint/next.js
@@ -1,18 +1,18 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
-import pluginNext from "@next/eslint-plugin-next";
-import { config as baseConfig } from "./base.js";
+import js from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
+import tseslint from "typescript-eslint"
+import pluginReactHooks from "eslint-plugin-react-hooks"
+import pluginReact from "eslint-plugin-react"
+import globals from "globals"
+import pluginNext from "@next/eslint-plugin-next"
+import baseConfig from "./base.js"
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
  *
  * @type {import("eslint").Linter.Config[]}
  * */
-export const nextJsConfig = [
+export default [
   ...baseConfig,
   js.configs.recommended,
   eslintConfigPrettier,
