b4220b79c6183aef1b7588814f0bbf9d754b8923
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-04-20 00:23:27 -0300

    refactor: remove organization slug from signOut redirect logic

--- apps/web/src/components/sign-out-button/actions.ts
+++ apps/web/src/components/sign-out-button/actions.ts
@@ -4,7 +4,6 @@ import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 
 export const signOut = async () => {
-  const slug = await auth.getCurrentOrganization()
   await auth.signOut()
-  redirect(`/${slug}/login`)
+  redirect(`/login`)
 }
