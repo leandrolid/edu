68c9f6ca34d1c83c4979e18483d17ed19885693e
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-06-15 21:18:45 -0300

    refactor: format rbacRoleSchema for improved readability

--- packages/rbac/src/types/role.type.ts
+++ packages/rbac/src/types/role.type.ts
@@ -1,18 +1,21 @@
 import z from 'zod'
 
-export const rbacRoleSchema = z.enum([
-  'OWNER',
-  'DEFAULT',
-  'ORGANIZATION_ADMIN',
-  'ORGANIZATION_CONTRIBUTOR',
-  'ORGANIZATION_MEMBER',
-  'ORGANIZATION_BILLING',
-  'TEAM_ADMIN',
-  'TEAM_CONTRIBUTOR',
-  'TEAM_MEMBER',
-  'MEMBER_ADMIN',
-  'MEMBER_CONTRIBUTOR',
-  'MEMBER',
-])
+export const rbacRoleSchema = z.enum(
+  [
+    'OWNER',
+    'DEFAULT',
+    'ORGANIZATION_ADMIN',
+    'ORGANIZATION_CONTRIBUTOR',
+    'ORGANIZATION_MEMBER',
+    'ORGANIZATION_BILLING',
+    'TEAM_ADMIN',
+    'TEAM_CONTRIBUTOR',
+    'TEAM_MEMBER',
+    'MEMBER_ADMIN',
+    'MEMBER_CONTRIBUTOR',
+    'MEMBER',
+  ],
+  { message: 'Permissão inválida' },
+)
 
 export type RbacRole = z.infer<typeof rbacRoleSchema>
