6c00c1f080c443ceee4e043d9b794357c591fc3a
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-06-16 08:59:52 -0300

    feat: enhance basic sign-in validation with custom error messages

--- apps/server/src/infra/http/controllers/auth/basic-sign-in/basic-sign-in.validation.ts
+++ apps/server/src/infra/http/controllers/auth/basic-sign-in/basic-sign-in.validation.ts
@@ -3,7 +3,9 @@ import z from 'zod'
 
 export class BasicSignInValidation implements IValidation {
   body: IValidator = z.object({
-    email: z.string().email(),
-    password: z.string().min(8),
+    email: z.string({ message: 'E-mail é obrigatório' }).email({ message: 'E-mail inválido' }),
+    password: z
+      .string({ message: 'Senha é obrigatória' })
+      .min(8, { message: 'Senha deve ter pelo menos 8 caracteres' }),
   })
 }
