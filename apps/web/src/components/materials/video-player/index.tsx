e4dec62cb5e72ba2ef569d810efac7c9952b0e32
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-06-23 08:59:27 -0300

    feat: integrate dashjs for video playback in VideoPlayer component

--- apps/web/src/components/materials/video-player/index.tsx
+++ apps/web/src/components/materials/video-player/index.tsx
@@ -1,49 +1,32 @@
 'use client'
 
-import { auth } from '@/auth'
-import { speedTest } from '@/http/services/materials/speed-test'
-import { streamVideo } from '@/http/services/materials/stream-video'
 import { AspectRatio, Flex } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
-import { useState } from 'react'
 
 export function VideoPlayer() {
-  const [videoSrc, setVideoSrc] = useState<string | undefined>()
-  const { data: networkSpeedMbps, isSuccess } = useQuery({
-    queryKey: ['networkSpeed'],
-    queryFn: async () => speedTest(),
-  })
-
   useQuery({
-    enabled: isSuccess,
     queryKey: ['video'],
     queryFn: async () => {
-      try {
-        const slug = await auth.getCurrentOrganization()
-        const response = await streamVideo({
-          slug: slug!,
-          videoId: 'bd99197d-1704-43dd-a9e0-15757c7744d9',
-          networkSpeedMbps: networkSpeedMbps || 0,
-        })
-        const url = URL.createObjectURL(response)
-        setVideoSrc(url)
-        return ''
-      } catch (error) {
-        console.error(error)
-        return ''
-      }
+      const dashjs = await import('dashjs')
+      await import('dashjs/mss')
+      let player = dashjs.MediaPlayer().create()
+      const container = document.querySelector<HTMLMediaElement>('#myMainVideoPlayer')
+      if (!container) return null
+      player.initialize(
+        container,
+        'http://localhost:3333/organizations/macedo-nogueira/videos/4abf5fa7-6347-493e-ba26-057febbc7973/manifest.mpd',
+        false,
+      )
+      return player
     },
   })
 
   return (
     <AspectRatio ratio={16 / 9} style={{ width: '100%' }}>
       <Flex direction="column" gap="4" width="100%">
-        {videoSrc && (
-          <video controls width="100%">
-            <source src={videoSrc} type="video/mp4" />
-            Seu navegador não suporta o elemento de vídeo.
-          </video>
-        )}
+        <video id="myMainVideoPlayer" controls width="100%">
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
       </Flex>
     </AspectRatio>
   )
