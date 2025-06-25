b7117d050bae045ad7bfd70598b0beb991258e7e
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-06-24 21:00:41 -0300

    refactor: simplify video upload processing and enhance event handling in ProcessVideoListener

--- apps/server/src/app/events/videos/process-video.listener.ts
+++ apps/server/src/app/events/videos/process-video.listener.ts
@@ -1,12 +1,14 @@
-import { BadRequestError, ConflictError, Inject, Injectable, OnEvent } from '@edu/framework'
+import {
+  BadRequestError,
+  ConflictError,
+  EVENT_SERVICE,
+  Inject,
+  Injectable,
+  OnEvent,
+  type IEventsService,
+} from '@edu/framework'
 import type { IStorageService } from '@infra/services/storage/storage.service'
 import type { IVideoService } from '@infra/services/video/video.service'
-
-export type PrepareStreamInput = {
-  id: string
-  slug: string
-  key: string
-}
 
 @Injectable()
 export class ProcessVideoListener {
@@ -15,20 +17,21 @@ export class ProcessVideoListener {
     private readonly videoService: IVideoService,
     @Inject('IStorageService')
     private readonly storageService: IStorageService,
+    @Inject(EVENT_SERVICE)
+    private readonly eventsService: IEventsService,
   ) {}
 
   @OnEvent('video.uploaded')
-  async execute({ id, key, slug }: PrepareStreamInput) {
-    console.log('Processing video', { id, key, slug })
-    const originalFile = await this.storageService.getOne(key)
-    const buffer = await originalFile.toBuffer()
-    const videoInfo = await this.videoService.getInfo({ buffer })
-    if (!videoInfo.audio) throw new BadRequestError('Vídeo sem áudio não é suportado')
-    const processor = await this.videoService.processFile({
-      buffer,
-      maxResolution: videoInfo.video.height,
-    })
+  async onVideoUploaded({ id, key }: { id: string; key: string }) {
     try {
+      const originalFile = await this.storageService.getOne(key)
+      const buffer = await originalFile.toBuffer()
+      const videoInfo = await this.videoService.getInfo({ buffer })
+      if (!videoInfo.audio) throw new BadRequestError('Vídeo sem áudio não é suportado')
+      const processor = await this.videoService.processFile({
+        buffer,
+        maxResolution: videoInfo.video.height,
+      })
       const fileDirectory = key.split('/').slice(0, -2).join('/')
       const uploads = await Promise.all(
         processor.files.map(async (file) => {
@@ -38,30 +41,48 @@ export class ProcessVideoListener {
           })
         }),
       )
-      const manifest = await this.videoService.createManifest({
+      await processor.close()
+      await this.eventsService.emit('video.processed', {
+        id,
+        key,
         maxResolution: videoInfo.video.height,
-        files: processor.files,
+        uploads: uploads.map((upload) => upload.key),
+      })
+    } catch (error) {
+      console.error(error)
+      throw new ConflictError('Erro ao processar o vídeo')
+    }
+  }
+
+  @OnEvent('video.processed')
+  async onVideoProcessed({
+    key,
+    uploads,
+    maxResolution,
+  }: {
+    id: string
+    key: string
+    maxResolution: number
+    uploads: string[]
+  }) {
+    try {
+      const files = await this.storageService.getMany(uploads)
+      const fileDirectory = key.split('/').slice(0, -2).join('/')
+      const manifest = await this.videoService.createManifest({
+        maxResolution,
+        files: files.map((file) => ({
+          name: file.key.split('/').pop() || '',
+          toStream: () => file.toStream(),
+        })),
       })
       const manifestUpload = await this.storageService.uploadStream({
         key: `${fileDirectory}/manifest.mpd`,
         stream: manifest.file.toStream(),
       })
-      await processor.close()
       await manifest.close()
-      console.log({
-        video: { id, slug },
-        uploads: uploads.map((upload) => upload.url),
-        manifestUrl: manifestUpload.url,
-      })
-      return {
-        video: { id, slug },
-        uploads: uploads.map((upload) => upload.url),
-        manifestUrl: manifestUpload.url,
-      }
     } catch (error) {
-      console.error(error)
-      processor.close()
-      throw new ConflictError('Erro ao processar o vídeo')
+      console.error('Error processing video:', error)
+      throw new BadRequestError('Erro ao processar o vídeo')
     }
   }
 }
