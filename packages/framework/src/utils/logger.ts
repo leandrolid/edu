6c8c57b7aa812f35a42e77f11242d3af077e4a69
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-06-27 20:57:49 -0300

    refactor: update Logger methods to improve message formatting consistency

--- packages/framework/src/utils/logger.ts
+++ packages/framework/src/utils/logger.ts
@@ -3,29 +3,29 @@ import chalk from 'chalk'
 export class Logger {
   constructor(private readonly prefix: string) {}
 
-  log(message: string, ...args: any[]) {
-    console.log(`[${this.prefix}] ${message}`, ...args)
+  log(message: any, ...args: any[]) {
+    console.log(`[${this.prefix}]`, message, ...args)
   }
 
-  info(message: string, ...args: any[]) {
-    console.info(chalk.blue(`[${this.prefix}] ${message}`), ...args)
+  info(message: any, ...args: any[]) {
+    console.info(chalk.blue(`[${this.prefix}]`, message), ...args)
   }
 
-  warn(message: string, ...args: any[]) {
-    console.warn(chalk.yellow(`[${this.prefix}] ${message}`), ...args)
+  warn(message: any, ...args: any[]) {
+    console.warn(chalk.yellow(`[${this.prefix}]`, message), ...args)
   }
 
-  error(message: string, ...args: any[]) {
-    console.error(chalk.red(`[${this.prefix}] ${message}`), ...args)
+  error(message: any, ...args: any[]) {
+    console.error(chalk.red(`[${this.prefix}]`, message), ...args)
   }
 
-  debug(message: string, ...args: any[]) {
+  debug(message: any, ...args: any[]) {
     if (process.env.NODE_ENV === 'development') {
-      console.debug(chalk.gray(`[${this.prefix}] ${message}`), ...args)
+      console.debug(chalk.gray(`[${this.prefix}]`, message), ...args)
     }
   }
 
-  success(message: string, ...args: any[]) {
-    console.log(chalk.green(`[${this.prefix}] ${message}`), ...args)
+  success(message: any, ...args: any[]) {
+    console.log(chalk.green(`[${this.prefix}]`, message), ...args)
   }
 }
