d3e0edcd90236889a325a1205ff95686bb1b150c
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-06-28 14:03:43 -0300

    refactor: replace HttpError with ServerError in HttpErrorHandler

--- apps/server/src/infra/http/errors/error.handler.ts
+++ apps/server/src/infra/http/errors/error.handler.ts
@@ -1,10 +1,10 @@
-import { HttpError, HttpStatusCode, IErrorHandler, type IResponse } from '@edu/framework'
+import { HttpStatusCode, IErrorHandler, ServerError, type IResponse } from '@edu/framework'
 import { hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod'
 
 export class HttpErrorHandler implements IErrorHandler {
   execute(error: Error, res: IResponse) {
     console.error(error)
-    if (error instanceof HttpError) {
+    if (error instanceof ServerError) {
       return res.status(error.statusCode).send({ message: error.message })
     }
     if (hasZodFastifySchemaValidationErrors(error)) {
