8b3c918e8a656e7b3c9ba760699f190a8bef0702
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-06-27 21:17:14 -0300

    feat: refactor VideoRepository to use IRepository for video creation

--- apps/server/src/infra/repositories/video/video.repository.imp.ts
+++ apps/server/src/infra/repositories/video/video.repository.imp.ts
@@ -1,5 +1,6 @@
+import type { IRepository } from '@domain/persistence/repository'
 import { Injectable } from '@edu/framework'
-import { prisma } from '@infra/database/connections/prisma.connection'
+import { InjectRepository } from '@infra/database/decorators/inject-repository'
 import type { CreateVideoInput, IVideoRepository } from '@infra/repositories/video/video.repository'
 import type { Video } from '@prisma/client'
 
@@ -7,18 +8,21 @@ import type { Video } from '@prisma/client'
   token: 'IVideoRepository',
 })
 export class VideoRepository implements IVideoRepository {
+  constructor(
+    @InjectRepository('Video')
+    private readonly repository: IRepository<Video>,
+  ) {}
+
   async create(input: CreateVideoInput): Promise<Video> {
-    return prisma.video.create({
-      data: {
-        id: input.id,
-        title: input.title,
-        ownerId: input.ownerId,
-        organizationId: input.organizationId,
-        assetId: input.assetId,
-        baseUrl: input.baseUrl,
-        duration: input.duration,
-        thumbnail: input.thumbnail,
-      },
+    return this.repository.createOne({
+      id: input.id,
+      title: input.title,
+      ownerId: input.ownerId,
+      organizationId: input.organizationId,
+      assetId: input.assetId,
+      baseUrl: input.baseUrl,
+      duration: input.duration,
+      thumbnail: input.thumbnail,
     })
   }
 }
