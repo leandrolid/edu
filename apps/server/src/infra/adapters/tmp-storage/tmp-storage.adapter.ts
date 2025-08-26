9e14a48e72ebdc78da8684296b026bd3ad5477bb
Leandro Liduvino <leandro.augusto460@gmail.com>
2025-08-25 22:47:57 -0300

    save

--- apps/server/src/infra/adapters/tmp-storage/tmp-storage.adapter.ts
+++ apps/server/src/infra/adapters/tmp-storage/tmp-storage.adapter.ts
@@ -20,22 +20,24 @@ export class TmpStorageAdapter {
     name: string
     close: () => void
   }> {
+    const tmpFile = fileSync({
+      name,
+      dir,
+      postfix: `.${extension}`,
+      keep: true,
+      discardDescriptor: true,
+      detachDescriptor: true,
+    })
+    const writeStream = createWriteStream(tmpFile.name)
+    stream.pipe(writeStream)
     return new Promise((resolve, reject) => {
-      const tmpFile = fileSync({
-        name,
-        dir,
-        postfix: `.${extension}`,
-        keep: true,
-      })
-      const writeStream = createWriteStream(tmpFile.name)
-      stream.pipe(writeStream)
-      writeStream.on('finish', () =>
+      writeStream.on('finish', () => {
         resolve({
           name: tmpFile.name,
           close: () => tmpFile.removeCallback(),
-        }),
-      )
-      writeStream.on('error', reject)
+        })
+      })
+      writeStream.on('error', (error) => reject(error))
     })
   }
 
--- apps/server/src/infra/services/jobs/create-manifest.job.ts
+++ apps/server/src/infra/services/jobs/create-manifest.job.ts
@@ -1,6 +1,13 @@
 import { VideoEvent } from '@app/events/videos/video.event'
 import type { IJobService, ProcessInput } from '@domain/services/queue.service'
-import { EVENT_SERVICE, Inject, Injectable, Logger, type IEventsService } from '@edu/framework'
+import {
+  EVENT_SERVICE,
+  Inject,
+  Injectable,
+  InternalServerError,
+  Logger,
+  type IEventsService,
+} from '@edu/framework'
 import { BullJobAdapter } from '@infra/adapters/bull/job.adapter'
 import { FfmpegBuilder } from '@infra/adapters/ffmpeg/ffmpeg.builder'
 import type { TmpStorageAdapter } from '@infra/adapters/tmp-storage/tmp-storage.adapter'
@@ -37,42 +44,50 @@ export class CreateManifestJob
   }
 
   async process({ data }: ProcessInput<CreateManifestInput>): Promise<void> {
-    const files = await this.storageService.getMany(data.assetsId)
     const tmpDir = await this.tmpStorage.createTempDir()
-    const tmpFiles = await Promise.all(
-      files.map((file) =>
-        this.tmpStorage.streamToTempFile({
-          name: file.key,
-          dir: tmpDir.name.split('/').pop(),
-          stream: file.toStream(),
-          extension: 'webm',
-        }),
-      ),
-    )
-    const ffmpeg = FfmpegBuilder.init(this.logger)
-      .toManifest(
-        tmpFiles.map((file) => file.name),
-        join(tmpDir.name, 'manifest.mpd'),
+    try {
+      const files = await this.storageService.getMany(data.assetsId)
+      const tmpFiles = await Promise.all(
+        files
+          .sort((a, b) => a.key.localeCompare(b.key))
+          .map((file) => {
+            return this.tmpStorage.streamToTempFile({
+              name: file.key.split('/').pop(),
+              dir: tmpDir.name.split('/').pop(),
+              stream: file.toStream(),
+              extension: 'webm',
+            })
+          }),
       )
-      .build()
-    await ffmpeg.toPromise()
-    const manifestBuffer = readFileSync(join(tmpDir.name, 'manifest.mpd'))
-    const manifest = await this.storageService.uploadOne({
-      key: this.videoAssetService.createPath({
-        slug: data.slug,
-        videoId: data.videoId,
-        name: 'manifest.mpd',
-      }),
-      buffer: manifestBuffer,
-    })
-    tmpDir.close()
-    await this.eventsService.emit(
-      VideoEvent.MANIFEST_CREATED,
-      VideoEvent.create({
-        assetId: manifest.key,
-        videoId: data.videoId,
-        slug: data.slug,
-      }),
-    )
+      const ffmpeg = FfmpegBuilder.init(this.logger)
+        .toManifest(
+          tmpFiles.map((file) => file.name),
+          join(tmpDir.name, 'manifest.mpd'),
+        )
+        .build()
+      await ffmpeg.toPromise()
+      const manifestBuffer = readFileSync(join(tmpDir.name, 'manifest.mpd'))
+      const manifest = await this.storageService.uploadOne({
+        key: this.videoAssetService.createPath({
+          slug: data.slug,
+          videoId: data.videoId,
+          name: 'manifest.mpd',
+        }),
+        buffer: manifestBuffer,
+      })
+      tmpDir.close()
+      await this.eventsService.emit(
+        VideoEvent.MANIFEST_CREATED,
+        VideoEvent.create({
+          assetId: manifest.key,
+          videoId: data.videoId,
+          slug: data.slug,
+        }),
+      )
+    } catch (error) {
+      tmpDir.close()
+      this.logger.error(error)
+      throw new InternalServerError('Failed to create video manifest')
+    }
   }
 }
